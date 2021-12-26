8c2bd3570c1fe4c3393ad450410690a86b84378e
Louis Lesueur <louis.lesueur@gmx.fr>
2021-12-26 10:45:43 +0100

    preparing the field for OcNet

---
+++ nets/deepSDF.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DeepSDF(nn.Module):
+
+    def __init__(self, code_dim=100, dim=3, n_layers=8, size=512):
+
+        super().__init__()
+
+        self.code_dim = code_dim
+        self.dim = dim
+
+        self.name = "DeepSDF"
+        self.known_shapes = 0
+        
+        self.input_layer = nn.utils.weight_norm(nn.Linear(dim+code_dim, size))
+
+        layers = []
+        for _ in range(n_layers):
+            layers.append(nn.utils.weight_norm(nn.Linear(size,size)))
+            layers.append(nn.ReLU())
+        self.hidden_layers = nn.Sequential(*layers)
+
+        self.output_layer = nn.utils.weight_norm(nn.Linear(size, 1))
+
+    def forward(self, x):
+        out = self.input_layer(x)
+        out = F.relu(out)
+        out = self.output_layer(out)
+        return torch.tanh(out)
--- train_deepSDF.py
+++ train_deepSDF.py
@@ -1,10 +1,10 @@
 import torch
 import os
 from tqdm import tqdm
-import argparse
+from torchvision.utils import make_grid
 from torch.utils.tensorboard import SummaryWriter
 import torch.optim as optim
-from nets import DeepSDF
+from nets.deepSDF import DeepSDF
 from dataset import PointCloudDataset
 from torch.utils.data import DataLoader, random_split
 import sys
@@ -75,3 +75,62 @@ for key, value in PARAMS.items():
     PARAM_TEXT += f"{key}: {value}\n"
 
 logging.info(f"Starting training, with parameters: \n{PARAM_TEXT}")
+
+
+
+def validation():
+    model.eval()
+    validation_loss = 0
+
+    with torch.no_grad():
+        for points, sdfs, indices in tqdm(val_loader):
+            points, sdfs, indices = points.to(device), sdfs.to(device), indices.to(device)
+            latent_vec = lat_vecs(indices)
+            data = torch.cat([points, latent_vec], dim=2)
+
+            for i, point in enumerate(data):
+                output = model(point)
+                validation_loss += criterion(output.T[0], sdfs[i])
+
+    return validation_loss / len(val_loader)
+
+
+if __name__ == "__main__":
+
+    iteration = 0
+    writer = SummaryWriter()
+    command = ''
+
+    writer.add_text("Params", PARAM_TEXT)
+
+    for epoch in range(1, PARAMS["epochs"]):
+        writer.add_scalar("Train/LR", optimizer.param_groups[0]["lr"], epoch)
+        model.train()
+
+        for batch_idx, (points, sdfs, indices) in enumerate(tqdm(train_loader)):
+            points, sdfs, indices = points.to(device), sdfs.to(device), indices.to(device)
+
+            latent_vec = lat_vecs(indices)
+            data = torch.cat([points, latent_vec], dim=2)
+            
+            output = model(data[0])
+            loss = criterion(output.T[0], sdfs[0]) / len(train_loader)
+            
+            writer.add_scalar("Train/Loss", loss.data.item(), iteration)
+
+            iteration += 1
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        val_loss = validation()
+        writer.add_scalar("Val/Loss", val_loss, epoch)
+        scheduler.step(val_loss)
+        model_file = os.path.join("checkpoints", f"{model.name}_{epoch}.pth")
+        torch.save({"model": model.state_dict(),
+                    "latent_vecs": lat_vecs.state_dict(),
+                    "latent_size": PARAMS["latent_size"],
+                    "shapes": model.known_shapes}, model_file)
+
+        logging.info(f"Training, epoch {epoch} finished, train loss: {loss.data.item()}, val loss: {val_loss}")
+
